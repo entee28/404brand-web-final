1eb5fd2ba8b127fb08b7920dbfb83834b624a18e
Thanh Nguyen <puffle.nnt@gmail.com>
2022-01-29 22:11:15 +0700

    added css snap scroll

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -57,53 +57,55 @@ const About = () => {
     }, []);
 
     return (
-        <div className='about' ref={el}>
+        <>
             <NavbarDark />
+            <div className='about' ref={el}>
 
-            <div className='particle-content-container'>
-                <div className='fancy-line'></div>
-                <div className="particle-content">
-                    <h1 id='particle-h1'>The One And <br /> Only</h1>
-                    <p id='particle-p'>Make scratches and fingerprints things of the past. <br /> In the future, there's only 404brand - your next dimension.</p>
-                </div>
-            </div>
-
-            <div className='particle-container' >
-                <Canvas dpr={Math.min(window.devicePixelRatio, 2)}>
-                    <pointLight position={[2, 3, 4]} />
-                    <perspectiveCamera position={[0, 0, 2]} fov={75} aspect={width / height} near={0.1} far={100} updateProjectionMatrix />
-                    <color attach='background' args={['#21282a']} />
-                    <Particle />
-                    <SmallParticle />
-                </Canvas>
-            </div>
-
-            <div className='mission-container' >
-                <div className='mission-img'></div>
-                <div className="mission-content">
-                    <h2>Our Mission</h2>
-                    <p className='pre-para'>404brand is the global leader in device customization. Founded on 11.11.11. Run by robots.</p>
-                    <p>We’re giving you two screen protectors with each order. That’s it. That’s the paragraph.</p>
-                </div>
-            </div>
-
-            <div className="sphere-container">
-                <div className='sphere-content'>
-                    <h1>Make Us Rich</h1>
+                <div className='particle-container' >
+                    <div className='particle-content-container'>
+                        <div className='fancy-line'></div>
+                        <div className="particle-content">
+                            <h1 id='particle-h1'>The One And <br /> Only</h1>
+                            <p id='particle-p'>Make scratches and fingerprints things of the past. <br /> In the future, there's only 404brand - your next dimension.</p>
+                        </div>
+                    </div>
+                    <Canvas dpr={Math.min(window.devicePixelRatio, 2)}>
+                        <pointLight position={[2, 3, 4]} />
+                        <perspectiveCamera position={[0, 0, 2]} fov={75} aspect={width / height} near={0.1} far={100} updateProjectionMatrix />
+                        <color attach='background' args={['#21282a']} />
+                        <Particle />
+                        <SmallParticle />
+                    </Canvas>
                 </div>
 
-                <Canvas dpr={Math.min(window.devicePixelRatio, 2)}>
-                    <pointLight position={[2, 3, 4]} color={0xffffff} intensity={0.1} />
-                    <pointLight position={[-1.86, 1, -1.65]} color={0xff0000} intensity={10} />
-                    <pointLight position={[2.13, -3, -1.98]} color={0xe1ff} intensity={6.8} />
-                    <perspectiveCamera position={[0, 0, 2]} fov={75} aspect={width / height} near={0.1} far={100} updateProjectionMatrix />
-                    <color attach='background' args={['#21282a']} />
-                    <Suspense fallback={null}>
-                        <Sphere />
-                    </Suspense>
-                </Canvas>
+                <div className='mission-container' >
+                    <div className='mission-img'></div>
+                    <div className="mission-content">
+                        <h2>Our Mission</h2>
+                        <p className='pre-para'>404brand is the global leader in device customization. Founded on 11.11.11. Run by robots.</p>
+                        <p>We’re giving you two screen protectors with each order. That’s it. That’s the paragraph.</p>
+                    </div>
+                </div>
+
+                <div className="sphere-container">
+                    <div className='sphere-content'>
+                        <h1>Make Us Rich</h1>
+                    </div>
+
+                    <Canvas dpr={Math.min(window.devicePixelRatio, 2)}>
+                        <pointLight position={[2, 3, 4]} color={0xffffff} intensity={0.1} />
+                        <pointLight position={[-1.86, 1, -1.65]} color={0xff0000} intensity={10} />
+                        <pointLight position={[2.13, -3, -1.98]} color={0xe1ff} intensity={6.8} />
+                        <perspectiveCamera position={[0, 0, 2]} fov={75} aspect={width / height} near={0.1} far={100} updateProjectionMatrix />
+                        <color attach='background' args={['#21282a']} />
+                        <Suspense fallback={null}>
+                            <Sphere />
+                        </Suspense>
+                    </Canvas>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 };
 
