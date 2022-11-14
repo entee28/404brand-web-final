49861070e5f70e1a70cc256ecf399bd0af9fa80f
Thanh Nguyen <puffle.nnt@gmail.com>
2022-11-15 01:12:45 +0700

    SHF-17: remove unused var

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,7 +34,6 @@ function App() {
   }, []);
 
   const auth = useSelector((state: RootState) => state.auth);
-  const userStore = useSelector((state: RootState) => state.userDetails);
   const { isAuthenticated, user } = auth;
 
   return (
